7fde9fab7b1a19cc07304b9128573c5b836db9de
ShiversVert <38329790+ShiversVert@users.noreply.github.com>
2018-09-27 17:35:11 +0200

    Add automate.h & types.h

---
+++ include/types.h
@@ -0,0 +1,23 @@
+/**
+ * @file types.h
+ * @author
+ * @brief Types definition
+ *
+ * Contains the definition of the differents types used in the program
+ */
+
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef enum {ETIQUETTE, COMMENTAIRE, DIRECTIVE, REGISTRE, VIRGULE, CHAINE, PARENTHESE, SAUT_LIGNE, DECIMAUX, HEXA, SYMBOLE} type_lexeme;
+/*Lexeme*/
+
+typedef struct _lexeme_ {
+		type_lexeme cat; 		/*category*/
+		double line_nb;			/*Line number*/
+		char* chain;			/*Exact chain*/
+	}LEXEME ;
+
+
+
+#endif /* _TYPES_H_ */
