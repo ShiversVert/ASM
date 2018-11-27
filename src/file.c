b0e2ac6d1511b19821d6bd093444325853743d2e
Romain BAILLY <bailly.romain@outlook.fr>
2018-11-27 23:05:07 +0100

    Livrable 3 : Fin de l'analyse grammaticale*

--- src/file.c
+++ src/file.c
@@ -14,20 +14,20 @@ int file_vide(File f){
  *@brief a utiliser de la maniere suivante : f = enfiler(p, f);
  */
 
-File enfiler(void* p, File f){						/*f = enfiler(pointeur vers l elem a enfiler, file actuelle)*/
+File enfiler(void* p, File f){				/*f = enfiler(pointeur vers l elem a enfiler, file actuelle)*/
 
 	File f_new = calloc(1, sizeof(*f_new)); /*creation d'un nouveau maillon*/
-	f_new->val = p;													/*On place le pointeur vers l'elem de la file*/
+	f_new->val = p;							/*On place le pointeur vers l'elem de la file*/
 
-	if(f == NULL){													/*Si la file actuelle est vide*/
-		f_new->suiv = f_new;									/*C'est le seul maillon de la file, il pointe donc sur lui meme*/
+	if(f == NULL){							/*Si la file actuelle est vide*/
+		f_new->suiv = f_new;				/*C'est le seul maillon de la file, il pointe donc sur lui meme*/
 	}
 
-	else{																		/*Sinon*/
-		f_new->suiv = f->suiv;								/*On insere le nouveau maillon a la fin de la file*/
+	else{									/*Sinon*/
+		f_new->suiv = f->suiv;				/*On insere le nouveau maillon a la fin de la file*/
 		f->suiv = f_new;
 	}
-	return f_new;														/*On retourne le nouveau maillon*/
+	return f_new;							/*On retourne le nouveau maillon*/
 }
 
 
@@ -51,9 +51,9 @@ void* defiler(File* f){
 	/*Sinon*/
 
 	File f_premier = (*f)->suiv;		/*On recupere le premier elem de la file*/
-	
+
 	void* p = f_premier->val;			/*On recupere l element du maillon qui va etre supprime*/
-	(*f)->suiv = f_premier->suiv;	/*On fait le nouveau branchement*/
+	(*f)->suiv = f_premier->suiv;		/*On fait le nouveau branchement*/
 	free(f_premier);					/*On libere le premier qui n est plus utilise*/
 	
 	return(p);
